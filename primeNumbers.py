61ba67f2e4081c535f289f601ee9f9f22b42a528
Nawire Sandra <nabwire001@gmail.com>
2017-07-03 18:11:03 +0300

    my python file

---
+++ primeNumbers.py
@@ -0,0 +1,10 @@
+def isPrime(n):
+    i=2
+    for p in range (2,n+1):
+        for i in range (2,p):
+            if p%2 == 0:
+                break 
+            else :
+                print (p)
+isPrime(10)
+
